61ba90d4cf8d38f46683d6570377285958029deb
Keval Shah <kjs503@nyu.edu>
2016-02-09 21:20:41 -0500

    Animation Done

--- ColorChecker/ColorChecker/ViewController.swift
+++ ColorChecker/ColorChecker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var colorLabel: UILabel!
     @IBOutlet var nameField: UITextField!
+    @IBOutlet var addName: UIButton!
     
     @IBAction func dismissKeyboard(sender: AnyObject)
     {
@@ -23,7 +24,9 @@ class ViewController: UIViewController {
         if let colors = (nameDictionary[string!]){
             if (colors.count > 0){
             let randomNumber: Int = Int(arc4random_uniform(UInt32(nameDictionary[string!]!.count)))
-            self.view.backgroundColor = nameDictionary[(nameField.text?.lowercaseString)!]![randomNumber]
+           //self.view.backgroundColor = nameDictionary[(nameField.text?.lowercaseString)!]![randomNumber]
+                UIView.animateWithDuration(1.0, animations: { () -> Void in
+                    self.view.backgroundColor = self.nameDictionary[(nameField.text?.lowercaseString)!]![randomNumber]}, completion: nil)
             colorLabel.hidden = true
             }}
             else {
@@ -31,6 +34,26 @@ class ViewController: UIViewController {
                 self.view.backgroundColor = UIColor.whiteColor()
             }
         
+    }
+    
+//    @IBAction func addNewName(sender: UIButton)
+//    {
+//        var newColor = getRandomColor()
+//        let string = nameField.text?.lowercaseString
+//        if (nameDictionary[string!] != nil){
+//            nameDictionary[string!].append(newColor)
+//            
+//        }
+//        else {
+//            nameDictionary[string!] = [newColor]
+//        }
+//    }
+    
+    func getRandomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
     
     var nameDictionary: [String:[UIColor]] = ["akshay": [UIColor(red: 0, green: 0,blue: 0, alpha: 1)],"paulina": [UIColor(red: 0, green: 0.5, blue: 0.5, alpha: 1)],"carlos": [UIColor(red: 0, green: 0.929, blue:1, alpha: 1)],"jayson": [UIColor(red: 0, green: 0.165, blue: 0.36, alpha: 1)],"nithi": [UIColor(red: 1, green: 0, blue: 0, alpha: 1)],"lorenzo": [UIColor(red: 0.663, green: 0.663, blue: 0.663, alpha: 1)],"david": [UIColor(red: 1, green: 0.714, blue: 0.757, alpha: 1), UIColor(red: 0.728, green: 0.855, blue: 0.333, alpha: 1)],"derek": [UIColor(red: 0.2, green: 0.275, blue: 0.349, alpha: 1)],"keval": [UIColor(red: 0.35,green: 0.01, blue: 0.05, alpha: 1)],"neill": [UIColor(red: 0.992, green: 0.875,blue: 0.975, alpha: 1)], "sam": [UIColor(red: 0.2,green: 0.4,blue: 1, alpha: 1)], "bobby": [UIColor(red: 0.625, green: 0.125, blue: 0.9375, alpha: 1)], "yuchi": [UIColor(red: 1, green: 0.337, blue: 0, alpha: 1)], "patrick": [UIColor(red: 0.141, green: 0.251,blue: 0.949, alpha: 1)]]
