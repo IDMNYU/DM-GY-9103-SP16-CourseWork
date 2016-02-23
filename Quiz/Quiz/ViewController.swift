2a68d254e5575b6746434650d2a1a5417bde0909
Keval Shah <Keval@172-16-204-244.DYNAPOOL.NYU.EDU>
2016-02-23 15:00:22 -0500

    Chapter 8 Complete with both Challenges

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     let questions: [String] = ["From what is cognac made?","What is 7+7?","What is the capital of Vermont?"]
     
@@ -20,7 +24,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
+    }
+    
+    func updateOffScreenLabel()
+    {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
     
     @IBAction func showNextQuestion(sender: AnyObject)
@@ -32,14 +44,44 @@ class ViewController: UIViewController {
         }
         
         let question:  String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer(sender: AnyObject)
     {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nextQuestionLabel.alpha = 0
+    
+    }
+    
+    func animateLabelTransitions()
+    {
+     //   let animationClosure = { () -> Void in
+       //     self.questionLabel.alpha = 1
+        //}
+        
+        view.layoutIfNeeded()
+        
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5,options: [.CurveLinear], animations: { self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded() },completion: {_ in swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+        swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+        self.updateOffScreenLabel()
+        })
     }
 }
 
