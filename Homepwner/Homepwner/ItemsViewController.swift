3f7e2337e5986dbe24ea7b7d911079fcf73c9c6c
yuchima <ym1356@nyu.edu.com>
2016-03-01 12:03:49 -0500

    page159

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -12,13 +12,29 @@ class ItemsViewController: UITableViewController{
     
     var itemStore: ItemStore!
     
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        //get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left:0, bottom:0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    
+    }
+    
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section:Int) ->Int {
         return itemStore.allItems.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         //create an instance of UITableViewCell, with default appearance
-        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        //let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+       
+        //get a new or recycled cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        
         
         //set the text on the cell with the description of the item
         //that is at the nth index of items, where n = row this cell
