a826e10cb8d620041302b8646fb78728702d5d68
Keval Shah <kjs503@nyu.edu>
2016-03-29 14:38:54 -0400

    Chap 15 commit with challenges

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -13,15 +13,55 @@ class ImageStore
     func setImage(image: UIImage, forKey key: String)
     {
         cache.setObject(image, forKey: key)
+        
+        let imageURL = imageURLForKey(key)
+        
+        if let data = UIImagePNGRepresentation(image)
+        {
+            data.writeToURL(imageURL, atomically: true)
+        }
     }
     
     func imageForKey(key: String) -> UIImage?
     {
-        return cache.objectForKey(key) as? UIImage
+        //return cache.objectForKey(key) as? UIImage
+        
+        if let existingImage = cache.objectForKey(key) as? UIImage
+        {
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key)
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else
+        {
+            return nil
+        }
+        cache.setObject(imageFromDisk, forKey: key)
+        return imageFromDisk
+    }
+    
+    func imageURLForKey(key: String) -> NSURL
+    {
+            let documentsDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        
+        return documentDirectory.URLByAppendingPathComponent(key)
     }
     
     func deleteImageForKey(key: String)
     {
         cache.removeObjectForKey(key)
+        
+        let imageURL = imageURLForKey(key)
+        do
+        {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        }
+        catch
+        {
+           // print("Error reading fromm the disk: \(error)")        }
+            print("Error removing the image from disk")
+        }
+    
     }
 }
